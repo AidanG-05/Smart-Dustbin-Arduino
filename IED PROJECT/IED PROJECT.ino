fb3190e3a03e0efa7f9163f7f11b6a7c8c659e61
LightningRWX <aidan.v.guna@gmail.com>
2023-01-19 20:53:57 +0800

    added ultrasound

--- IED PROJECT/IED PROJECT.ino
+++ IED PROJECT/IED PROJECT.ino
@@ -1,7 +1,9 @@
 #include <LiquidCrystal_I2C.h>
-#include "../../../../Documents/Arduino/libraries/LiquidCrystal_I2C-1.1.2/LiquidCrystal_I2C.h"
+#define TrigPin1 11   // U/S1 Trig connected to pin 11
+#define EchoPin1 10   // U/S1 Echo connected to pin 10
 #define Hit_aPin 4;
 #define Hit_bPin 2;
+
 LiquidCrystal_I2C lcd(0x27, 16, 2);
 
 
@@ -13,6 +15,9 @@ void setup() {
     Serial.begin(9600);
     pinMode(2, INPUT_PULLUP);
     pinMode(4, INPUT_PULLUP);
+    pinMode(TrigPin1, OUTPUT);  //Arduino's output, ranger's input
+    pinMode(EchoPin1, INPUT);   //Arduino's input, ranger's output
+
     PCICR |= B00000100; // Enable interrupts on PD port
     PCMSK2 |= B00010100; // Trigger interrupts on pins D2 and D4
     lcd.init();
@@ -29,6 +34,7 @@ void loop()
     lcd.setCursor(0, 1);
     delay(1000);
 
+    ultrasound1();
 }
 
 ISR(PCINT2_vect) {
@@ -39,4 +45,32 @@ ISR(PCINT2_vect) {
     else if (digitalRead(startStop) == LOW) {
 
     }
+}
+
+
+void ultrasound1() {
+    long pulseDuration; //variable needed by the ultrasound sensor code
+    int Distance;       // Ultrasound distance in cm
+
+    // this main code runs repeatedly:
+    // 1. Produce a 15us (micro-second) HIGH pulse in Trig to trigger the sensor...
+    digitalWrite(TrigPin1, LOW);
+    delayMicroseconds(2);
+    digitalWrite(TrigPin1, HIGH);
+    delayMicroseconds(5);
+    digitalWrite(TrigPin1, LOW);
+
+    // 2. Use pulseIn() function to measure the duration of the HIGH pulse in Echo 
+    // every 58 us is an obstacle distance of 1 cm
+
+    pulseDuration = pulseIn(EchoPin1, HIGH);
+    Distance = pulseDuration / 58;  //  Convert to cm
+
+    // 3. display the obstacle distance in serial monitor 
+    Serial.print("Distance = ");
+    Serial.print(Distance);
+    Serial.println(" cm");
+    delay(500);
+
+    
 }
