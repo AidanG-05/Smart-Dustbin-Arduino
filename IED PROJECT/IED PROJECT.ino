f195bb96d437a93d4d78e60e58d7a24aae40cd6b
LightningRWX <aidan.v.guna@gmail.com>
2023-01-19 19:02:21 +0800

    lcd integrated partly with target board

--- IED PROJECT/IED PROJECT.ino
+++ IED PROJECT/IED PROJECT.ino
@@ -1,33 +1,42 @@
-#define Hit_aPin 4
-#define Hit_bPin 2
-int counterA = 0;
-int counterB = 0;
+#include <LiquidCrystal_I2C.h>
+#include "../../../../Documents/Arduino/libraries/LiquidCrystal_I2C-1.1.2/LiquidCrystal_I2C.h"
+#define Hit_aPin 4;
+#define Hit_bPin 2;
+LiquidCrystal_I2C lcd(0x27, 16, 2);
+
+
+int reset = 0;
+int startStop = 0;
 
 // the setup function runs once when you press reset or power the board
 void setup() {
     Serial.begin(9600);
-    pinMode(counterA, INPUT_PULLUP);
-    pinMode(counterB, INPUT_PULLUP);
-    PCICR = B00000100;
-    PCMSK2 = B00010100;
-
-
+    pinMode(2, INPUT_PULLUP);
+    pinMode(4, INPUT_PULLUP);
+    PCICR |= B00000100; // Enable interrupts on PD port
+    PCMSK2 |= B00010100; // Trigger interrupts on pins D2 and D4
+    lcd.init();
+    lcd.begin(16, 2);
+    lcd.backlight();
+    lcd.clear();
 }
 
 // the loop function runs over and over again until power down or reset
-void loop() {
-    Serial.print("hit A score is: ");
-    Serial.println(counterA);
-    Serial.print("hit B score is: ");
-    Serial.println(counterB);
-    Serial.println("ready for next hit");
+void loop()
+{
+    lcd.setCursor(0, 0);
+    lcd.print("Free to fill");
+    lcd.setCursor(0, 1);
     delay(1000);
+
 }
 
 ISR(PCINT2_vect) {
-    if (digitalRead(counterA) == LOW) {
-        counterA += 1;
+    if (digitalRead(reset) == LOW)
+    {
+        Serial.print("Free to fill");
     }
-    else if (digitalRead(counterB) == LOW) {
-        counterB += 1;
+    else if (digitalRead(startStop) == LOW) {
+
     }
+}
